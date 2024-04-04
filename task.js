965c57758656063a126203d03a8bde633bb9300a
vijayalakshmixy <159040500+vijayalakshmixy@users.noreply.github.com>
2024-04-04 17:27:21 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,92 @@
+
+
+// let button=document.querySelector('button');
+// button.addEventListener("click",()=>{
+//     console.log("button clicked");
+//     document.div.style.background="red";
+// });
+
+let a=document.getElementById("box");
+let f=document.getElementById("red");
+f.addEventListener("click",()=>{
+    a.style.backgroundColor="red";
+    a.style.color="white"
+
+})
+
+let a1=document.getElementById("box");
+let f1=document.getElementById("yellow");
+f1.addEventListener("click",()=>{
+    a1.style.backgroundColor="yellow";
+    a1.style.color="black"
+
+})
+
+let a2=document.getElementById("box");
+let f2=document.getElementById("green");
+f2.addEventListener("click",()=>{
+    a2.style.backgroundColor="green";
+
+})
+
+let a3=document.getElementById("box");
+let f3=document.getElementById("pink");
+f3.addEventListener("click",()=>{
+    a3.style.backgroundColor="pink";
+    a3.style.color="red";
+
+})
+
+let a4=document.getElementById("box");
+let f4=document.getElementById("orange");
+f4.addEventListener("click",()=>{
+    a4.style.backgroundColor="orange";
+    a4.style.color="black"
+
+});
+
+let a5=document.getElementById("box");
+let f5=document.getElementById("purple");
+f5.addEventListener("click",()=>{
+    a5.style.backgroundColor="purple";
+    a5.style.color="white";
+
+});
+
+let a6=document.getElementById("box");
+let f6=document.getElementById("black");
+f6.addEventListener("click",()=>{
+    a6.style.backgroundColor="black";
+    a6.style.color="white";
+
+});
+
+let a7=document.getElementById("box");
+let f7=document.getElementById("white");
+f7.addEventListener("click",()=>{
+    a7.style.backgroundColor="white";
+    a7.style.color="black";
+
+})
+
+let a8=document.getElementById("box");
+let f8=document.getElementById("aqua");
+f8.addEventListener("click",()=>{
+    a8.style.backgroundColor="aqua";
+    a8.style.color="black";
+
+});
+
+let a9=document.getElementById("box");
+let f9=document.getElementById("grey");
+f9.addEventListener("click",()=>{
+    a9.style.backgroundColor="grey";
+    a9.style.color="red";
+
+})
+
+
+
+
+
+
